dd2feef6076025b8b90ce3b0ebc8f05a0f13c4f4
Alex <alex.phan2905@gmail.com>
2022-05-07 16:24:37 +0930

    hide back to top button until the window is scrolled past a certain point

--- dist/bundle.js
+++ dist/bundle.js
@@ -4338,13 +4338,29 @@ window.bootstrap.Dropdown = require('bootstrap/js/dist/dropdown');
 })();
 
 function scrollToTop() {
-    var backToTopButton = document.querySelector('.back-to-top button');
+    var backToTop = document.querySelector(".back-to-top");
+    var backToTopButton = document.querySelector(".back-to-top button");
+    var scrollSensor = 500;
 
-    backToTopButton.addEventListener('click', function() {
+    backToTopButton.addEventListener("click", function() {
         scroll({
             top: 0,
             left: 0,
-            behavior: 'smooth',
+            behavior: "smooth",
         });
     });
+
+    window.addEventListener("scroll", function () {
+        showButton();
+    });
+
+    function showButton() {
+        if (window.pageYOffset > scrollSensor) {
+            backToTop.classList.remove("d-none");
+            backToTop.classList.add("d-block");
+        } else if (window.pageYOffset <= scrollSensor) {
+            backToTop.classList.remove("d-block");
+            backToTop.classList.add("d-none");
+        }
+    }
 }
